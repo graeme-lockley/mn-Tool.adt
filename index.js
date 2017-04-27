2bad5ab5ca9e9be07524571afe74415bebfdc0e5
Graeme Lockley <graemel@no9.co.za>
2017-04-27 09:18:55 +0200

    refactor: make use of System.IO.Native.SyncFileSystem

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
-const FS = require("fs");
 const Lexer = require("./Tool/ADT/Lexer");
 const Parser = require("./Tool/ADT/Parser");
 const Result = mrequire("core:Data.Result:1.0.0");
+const SyncFileSystem = mrequire("core:System.IO.Native.SyncFileSystem:1.0.0");
 const Translate = require("./Tool/ADT/Translate");
 
 
-function compile(sourceName, targetName) {
-    return loadFile(sourceName)
+const compile = (sourceName, targetName) =>
+    SyncFileSystem.readFile(sourceName)
         .andThen(content => Parser.parse(Lexer.fromString(content)))
         .andThen(ast => Translate(ast.first))
-        .andThen(content => writeFile(targetName, content));
-}
-
-
-function loadFile(fileName) {
-    try {
-        return Result.Okay(FS.readFileSync(fileName));
-    } catch (e) {
-        return Result.Error(e.toString());
-    }
-}
-
-
-function writeFile(fileName, content) {
-    try {
-        return Result.Okay(FS.writeFileSync(fileName, content));
-    } catch (e) {
-        return Result.Error(e.toString());
-    }
-}
+        .andThen(writeFile(targetName));
 
 
 module.exports = compile;
